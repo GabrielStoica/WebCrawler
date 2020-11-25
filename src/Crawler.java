860676c8075310d33011cbe9b37db1bc6623963d
marius-gabriel <marius_gabriel1998@yahoo.com>
2020-11-25 12:27:53 +0200

    Redenumire clasa Main
    Pentru utilizarea corecta a utilitarului din linie de comanda, clasa Main devine Crawler
    
    Signed-off-by: marius-gabriel <marius_gabriel1998@yahoo.com>

--- src/Crawler.java
+++ src/Crawler.java
@@ -3,7 +3,7 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 
-public class Main {
+public class Crawler {
     public static void main(String[] args){
 
         System.out.println("Test citire cod sursa HTML al unui site");
